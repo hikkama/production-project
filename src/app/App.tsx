367fdbac42629a5749d9a45793e426f83152b77b
hikkama <khasanov.nurlan@gmail.com>
2023-02-08 21:50:09 +0300

    add ThemeSwitcher, Button, some loaders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom'
-
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
 import { AppRouter } from './providers/router'
@@ -8,13 +6,12 @@ import { Navbar } from 'widgets/Navbar'
 import './styles/index.scss'
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme()
+  const { theme } = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toggleTheme}>Toggle</button>
     </div>
   )
 }
