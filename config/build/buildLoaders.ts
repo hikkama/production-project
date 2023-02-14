a28e817d9d0428b2101752577c2cca0464317a6a
hikkama <khasanov.nurlan@gmail.com>
2023-02-14 17:34:50 +0300

    add babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,17 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     use: ['@svgr/webpack'],
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  }
+
   const cssLoaders = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -55,5 +66,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   }
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoaders]
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoaders]
 }
