7214c5aaf9607ef0c6b8ecd6928cc115b94e4767
hikkama <khasanov.nurlan@gmail.com>
2023-02-19 10:49:45 +0300

    setup test enviroment for components

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,6 +4,13 @@ export default {
   coveragePathIgnorePatterns: ['/node_modules/'],
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  modulePaths: [
+    '<rootDir>/src/',
+  ],
+	moduleNameMapper: {
+    '\\.(s?css)$': 'identity-obj-proxy',
+  },
   rootDir: '../../',
   testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTests.ts'],
 }
