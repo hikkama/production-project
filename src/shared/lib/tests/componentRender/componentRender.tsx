f27c767aaefd5c8614518bcd9e2a24c340435189
hikkama <khasanov.nurlan@gmail.com>
2023-02-20 11:40:08 +0300

    fix tests, fix scss

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -4,12 +4,12 @@ import { I18nextProvider } from 'react-i18next'
 import { MemoryRouter } from 'react-router-dom'
 import i18nForTests from 'shared/config/i18n/i18nForTests'
 
-export interface componentRenderOptions {
-	route: string
+export interface ComponentRenderOptions {
+	route?: string
 }
 
-export function renderWithTranslation(component: ReactNode, options: componentRenderOptions) {
-  const { route } = options
+export function componentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+  const { route = '/' } = options
   return render(
     <MemoryRouter initialEntries={[route]}>
       <I18nextProvider i18n={i18nForTests}>
