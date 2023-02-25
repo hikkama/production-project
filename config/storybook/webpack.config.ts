da425c3e17f84d1e32112607c19d5a4418e5eb5e
hikkama <khasanov.nurlan@gmail.com>
2023-02-25 15:43:47 +0300

    Trying to fix bug with folder AuthByUsername

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -29,7 +29,6 @@ export default ({ config }: {config: webpack.Configuration}) => {
     use: ['@svgr/webpack'],
   })
   config.module.rules.push(buildCssLoader(true))
-
   config.plugins?.push(new DefinePlugin({
     __IS_DEV__: true,
   }))
